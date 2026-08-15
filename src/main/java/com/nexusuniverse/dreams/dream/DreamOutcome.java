aa11996abee18ddb8258d241f41a86a2ff1d9a49
NathansGaming <99851331+NathansGaming@users.noreply.github.com>
2026-08-15 12:15:58 -0400

    Delete src/main directory

--- src/main/java/com/nexusuniverse/dreams/dream/DreamOutcome.java
+++
@@ -1,4 +0,0 @@
-package com.nexusuniverse.dreams.dream;
-
-public record DreamOutcome(DreamTier tier, DreamFlavor flavor, double strain) {
-}
--- src/main/java/com/nexusuniverse/dreams/dream/DreamTier.java
+++
@@ -1,9 +0,0 @@
-package com.nexusuniverse.dreams.dream;
-
-/** How mechanically significant a dream is, driven by the player's overall strain score. */
-public enum DreamTier {
-    PEACEFUL,
-    RESTLESS,
-    TROUBLED,
-    NIGHTMARE
-}
