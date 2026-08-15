8f6b4f05e661344889519b483d2755af923d0208
NathansGaming <99851331+NathansGaming@users.noreply.github.com>
2026-08-14 21:48:20 -0400

    Delete src/main directory

--- src/main/java/com/nexus/serverrules/detection/PatternEntry.java
+++
@@ -1,7 +0,0 @@
-package com.nexus.serverrules.detection;
-
-public record PatternEntry(String rootTerm, String normalizedRoot, ViolationCategory category) {
-    public static PatternEntry of(String rootTerm, ViolationCategory category) {
-        return new PatternEntry(rootTerm, TextNormalizer.normalize(rootTerm), category);
-    }
-}
